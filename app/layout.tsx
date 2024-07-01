1498f02273310ab61d6f11bedc75619536fe8957
Adam Winfield-Smith <adamws@hawaii.edu>
2024-06-30 15:09:25 -1000

    edited metadata and resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,16 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "adamwins",
-    template: "%s | chronark.com",
+    default: "adamws",
+    template: "%s | adamws.com",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Adam Winfield-Smith's Professional Portfolio",
   openGraph: {
-    title: "chronark.com",
+    title: "adamws.com",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
-    images: [
-      {
-        url: "https://chronark.com/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
+      "Computer Science Student at the University of Hawaii at Manoa",
+    url: "https://adamws.com",
+    siteName: "adamws.com",
     locale: "en-US",
     type: "website",
   },
@@ -36,10 +29,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
