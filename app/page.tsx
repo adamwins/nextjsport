b6b4e329168070286451cb77547d4cb92bfb1c13
Adam Winfield-Smith <adamws@hawaii.edu>
2024-07-02 08:44:33 -1000

    Updated so that animation does not happen on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import Link from "next/link";
-import Particles from "./components/particles";
-import TypedHome from "./components/typed";
+"use client"
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Particles from './components/particles';
+import TypedHome from './components/typed';
 
 const navigation = [
-  { name: "About Me", href: "/aboutme" },
-  { name: "Projects", href: "/projects" },
-  { name: "Resume", href: "/resume" },
-  { name: "Contact Me", href: "/contact" },
+  { name: 'About Me', href: '/aboutme' },
+  { name: 'Projects', href: '/projects' },
+  { name: 'Resume', href: '/resume' },
+  { name: 'Contact Me', href: '/contact' },
 ];
 
+function isMobile() {
+  return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop|webOS|BlackBerry/i.test(navigator.userAgent);
+}
+
 export default function Home() {
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    setIsMobileDevice(isMobile());
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
@@ -28,9 +41,13 @@ export default function Home() {
       <div className="hidden w-screen h-px md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={50} 
+        quantity={isMobileDevice ? 50 : 100} 
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <h1
+        className={`z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ${
+          !isMobileDevice ? 'animate-title' : ''
+        }`}
+      >
         Hi, I'm Adam.
       </h1>
 
@@ -41,4 +58,3 @@ export default function Home() {
     </div>
   );
 }
-
