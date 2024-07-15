53dde62b4e5ee624f7d0a312741383fdba4b6cb4
Adam Winfield-Smith <adamws@hawaii.edu>
2024-07-15 10:24:22 -1000

    Changed the way that the used device is detected to match the about me page

--- app/components/clientcomponent.jsx
+++ app/components/clientcomponent.jsx
@@ -9,28 +9,17 @@ const fadeIn = {
     visible: { opacity: 1, transition: { duration: 1.5 } }
 };
 
+const isMobile = () => {
+    return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop|webOS|BlackBerry/i.test(navigator.userAgent);
+};
+
 const AboutMeClient = () => {
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobileDevice, setIsMobileDevice] = useState(false);
     const [hasMounted, setHasMounted] = useState(false);
 
     useEffect(() => {
         setHasMounted(true);
-
-        const checkIfMobile = () => {
-            setIsMobile(window.matchMedia('(max-width: 768px)').matches);
-        };
-
-        checkIfMobile();
-
-        const resizeListener = () => {
-            checkIfMobile();
-        };
-
-        window.addEventListener('resize', resizeListener);
-
-        return () => {
-            window.removeEventListener('resize', resizeListener);
-        };
+        setIsMobileDevice(isMobile());
     }, []);
 
     return (
@@ -53,47 +42,47 @@ const AboutMeClient = () => {
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" 
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain-wordmark.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-plain-wordmark.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-plain-wordmark.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                     </div>
                     <h1 className="text-xl md:text-2xl font-bold mb-4 pt-6 md:pt-10">Currently Learning:</h1>
@@ -101,24 +90,24 @@ const AboutMeClient = () => {
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                         <motion.img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swift/swift-original.svg" 
                             className="h-16 md:h-20 w-16 md:w-20"
-                            whileHover={!isMobile ? { scale: 1.1 } : {}}
+                            whileHover={!isMobileDevice ? { scale: 1.1 } : {}}
                         />
                     </div>
                 </motion.div>
                 <motion.div
                     className="w-full md:w-1/2 flex justify-center md:justify-center p-4 md:p-10"
-                    initial={!isMobile ? { scale: 0.8 } : {}}
-                    animate={!isMobile ? { scale: 1, transition: { duration: 1.5 } } : {}}
+                    initial={!isMobileDevice ? { scale: 0.8 } : {}}
+                    animate={!isMobileDevice ? { scale: 1, transition: { duration: 1.5 } } : {}}
                 >
                     <img src="/aboutme.png" alt="Image" className="w-90 h-90 md:w-90 md:h-90 rounded-full shadow" />
                 </motion.div>
